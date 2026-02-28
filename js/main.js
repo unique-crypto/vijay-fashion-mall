81bb0529391a5d82354772ef80d856683e5f8783
Swaraj Lahoti <swarajlahoti06@gmail.com>
2026-02-28 15:17:50 +0530

    fix: resolve loading issue on homepage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// Redirect to home page after 3 seconds
-function homenavigate() {
-setTimeout(function() {
-    window.location.href = "home.html";
-}, 3000);
-}
 // Hamburger Menu Toggle
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.querySelector(".nav-links");
